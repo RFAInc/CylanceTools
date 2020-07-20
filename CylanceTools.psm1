3bcd806216504dda17fdc97b87c9496f0b717eb9
Tony Pagliaro <tony@pagliaro.co>
2020-07-20 13:11:25 -0400

    Moved new function to module. After testing seems to produce a good uninsall string, given 3 cases.

--- CylanceTools.psm1
+++ CylanceTools.psm1
@@ -66,3 +66,116 @@ function Set-CylanceRegistration
         }
     }
 }
+
+# DRAFT
+
+
+function Uninstall-Cylance {
+    <#
+    .SYNOPSIS
+    Removes the application from the local system.
+    .DESCRIPTION
+    Searches the local registry for an uninstall string, classifies the string based on path or GUID, and executes a command with silent options.
+    .NOTES
+    Examples of actual uninstall strings found on devices:
+    Logs of attempts are showing the following for the invoked:
+
+    msiexec.exe /X "C:\ProgramData\Package Cache{0074de7b-882e-42ef-bf12-6da746eeb15f}\CylanceProtectSetup.exe" /modify
+
+    This worked:
+
+    "C:\ProgramData\Package Cache{0074de7b-882e-42ef-bf12-6da746eeb15f}\CylanceProtectSetup.exe" /uninstall
+    #>
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+
+        # Load some external functions
+        $web = New-Object Net.WebClient
+        $TheseFunctionsForPstFileInfo = @(
+            'https://raw.githubusercontent.com/tonypags/PsWinAdmin/master/Get-InstalledSoftware.ps1'
+        )
+        Foreach ($uri in $TheseFunctionsForPstFileInfo) {
+            $web.DownloadString($uri) | Invoke-Expression
+        }
+        $web.Dispose | Out-Null
+
+        # Define cases
+        $Cases = @(
+            [pscustomobject]@{
+                Case = 1
+                Pattern = $null
+                Like = 'MsiExec.exe /X{*'
+                Replacement = '{0} /qn /norestart'
+            }
+            [pscustomobject]@{
+                Case = 2
+                Pattern = '\s\/modify'
+                Like = $null
+                Replacement = ' /uninstall'
+            }
+            [pscustomobject]@{
+                Case = 3
+                Pattern = $null
+                Like = '*.exe"* /uninstall'
+                Replacement = $null
+            }
+        )#END $Cases
+
+        # Get uninstall string for Cylance
+        $strUninstall = Get-InstalledSoftware |
+            Where-Object {$_.Name -like 'Cylance*'} |
+            Select-Object -ExpandProperty UninstallCommand
+        Write-Debug "found $($strUninstall)"
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+        # Test to see which case
+        $foundCase = $null
+        foreach ($Case in $Cases) {
+            $thisCase = $Case.Case
+            if ($strUninstall -match ($Case.Pattern) -and ($Case.Pattern)) {
+                Write-Debug "match case"
+                $foundCase = $thisCase
+                continue
+            } elseif ($strUninstall -like ($Case.Like) -and ($Case.Like)) {
+                Write-Debug "like case"
+                $foundCase = $thisCase
+                continue
+            }
+        }#END foreach ($Case in $Cases)
+
+        # Isolate my case
+        $objCase = $Cases | Where-Object {$_.Case -eq $foundCase}
+
+        switch ($foundCase) {
+            1 {
+                # Take existing string and append to it
+                $strFinalString = ($objCase.Replacement) -f ($strUninstall)
+            }
+            2 {
+                # Replace case pattern with replacement
+                $strFinalString = $strUninstall -replace ($objCase.Pattern), ($objCase.Replacement)
+            }
+            3 {
+                # This case requires no modification
+                $strFinalString = $strUninstall
+            }
+            Default {throw 'unhandled case'}
+        }#END switch ($foundCase)
+
+        # Execute uninstaller string
+        Write-Verbose $strFinalString
+        Write-Debug "final string ready"
+        & { $strFinalString }
+
+    }#END end
+}
