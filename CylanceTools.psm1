270ebae2924628a7286e0020396387c11fa00058
Zachary Barton <zbarton@rfa.com>
2020-12-17 15:10:31 -0500

    commenting out verbose logging for PsV2

--- CylanceTools.psm1
+++ CylanceTools.psm1
@@ -212,7 +212,7 @@ function Get-CylanceUninstallString {
         $strUninstall = Get-InstalledSoftware |
             Where-Object {$_.Name -like 'Cylance*'} |
             Select-Object -ExpandProperty UninstallCommand
-        Write-Verbose "Uninstall string found: $($strUninstall)" -Verbose:$Verbose
+        #Write-Verbose "Uninstall string found: $($strUninstall)" -Verbose:$Verbose
     }
     
     process {
@@ -237,7 +237,7 @@ function Get-CylanceUninstallString {
         # Isolate my case
         $objCase = $Cases | Where-Object {$_.Case -eq $foundCase}
 
-        Write-Verbose "Processing final Uninstall string..." -Verbose:$Verbose
+        #Write-Verbose "Processing final Uninstall string..." -Verbose:$Verbose
         switch ($foundCase) {
             1 {
                 # Extract the GUId from the uninstall string
