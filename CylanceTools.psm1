7c2dc8d622077f1456a6249c81d5aebdbe4dcd16
Zachary Barton <zbarton@rfa.com>
2020-12-17 14:53:56 -0500

    Debug PSv2 change

--- CylanceTools.psm1
+++ CylanceTools.psm1
@@ -167,7 +167,7 @@ function Get-CylanceUninstallString {
         # Path to log file for MSI case only
         [Parameter()]
         [string]
-        $LogPath = 'c:\windows\temp\cylance-remove.log',
+        $LogPath = 'c:\windows\temp\cylance-remove.log'#,
         
 		#This line doesn't play nice with Powershell v2, removing it for testing 
         #[switch]$Verbose
